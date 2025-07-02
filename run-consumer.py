4f0cfcc20a1f7e0f31a782f1f2b12b4e860f6c18
palkamarlene <marlene.palka@zalf.de>
2025-07-02 09:59:57 +0200

    Add cummulative ET output

--- run-consumer.py
+++ run-consumer.py
@@ -91,6 +91,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
         # "N2O": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "NH3": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         # "CumET": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "CumET": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
         "CumIRR": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1}
     }
     output_keys = list(output_grids.keys())
